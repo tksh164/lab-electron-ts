e99c4629598e19b21d420aa2d17502d428ffde63
Takeshi Katano <takatano@outlook.com>
2019-08-24 23:29:19 +0900

    Change app class name

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,23 @@
 import { app, BrowserWindow } from 'electron';
 import * as path from 'path';
 
-class Main {
+class App {
     // Keep on the reference of the main window object.
     static mainWindow: Electron.BrowserWindow | null;
     static application: Electron.App;
     static BrowserWindow: typeof BrowserWindow;
 
     static main (app: Electron.App, browserWindow: typeof BrowserWindow) {
-        Main.BrowserWindow = browserWindow;
-        Main.application = app;
-        Main.application.on('window-all-closed', Main.onWindowAllClosed);
-        Main.application.on('activate', Main.onActivate);
-        Main.application.on('ready', Main.onReady);
+        App.BrowserWindow = browserWindow;
+        App.application = app;
+        App.application.on('window-all-closed', App.onWindowAllClosed);
+        App.application.on('activate', App.onActivate);
+        App.application.on('ready', App.onReady);
     }
 
     private static createWindow() {
         // Create the browser window.
-        Main.mainWindow = new Main.BrowserWindow({
+        App.mainWindow = new App.BrowserWindow({
             width: 800,
             height: 600,
             webPreferences: {
@@ -26,35 +26,35 @@ class Main {
         });
     
         // and load the index.html of the app.
-        Main.mainWindow.loadFile(path.join(__dirname, '../index.html'));
+        App.mainWindow.loadFile(path.join(__dirname, '../index.html'));
     
         // Open the DevTools.
         //Main.mainWindow.webContents.openDevTools();
     
         // Emitted when the window is closed.
-        Main.mainWindow.on('closed', Main.onClosed);
+        App.mainWindow.on('closed', App.onClosed);
     }
 
     private static onReady() {
-        Main.createWindow();
+        App.createWindow();
     }
 
     private static onClosed() {
         // Dereference the main window object.
-        Main.mainWindow = null;
+        App.mainWindow = null;
     }
 
     private static onWindowAllClosed () {
         // On macOS it is common for applications and their menu bar
         // to stay active until the user quits explicitly with Cmd+Q
         if (process.platform !== 'darwin') {
-            Main.application.quit();
+            App.application.quit();
         }
     }
 
     private static onActivate() {
-        if (Main.mainWindow === null) {
-            Main.createWindow();
+        if (App.mainWindow === null) {
+            App.createWindow();
         }
     }
 }
@@ -62,4 +62,4 @@ class Main {
 // In this file you can include the rest of your app's specific main process code.
 // You can also put them in separate file and require them here.
 
-Main.main(app, BrowserWindow);
+App.main(app, BrowserWindow);
